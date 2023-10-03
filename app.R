3f46e7985ccf3442c09cd2331fe7406ac4f548a8
suwooh <ogorolevis@gmail.com>
2023-10-03 09:43:31 +0300

    listening 0.0.0.0

--- app.R
+++ app.R
@@ -103,7 +103,7 @@ source("product.R")
 source("cata.R")
 source("catalogue.R")
 
-#options(shiny.host = '0.0.0.0')
+options(shiny.host = '0.0.0.0')
 #options(shiny.port = 8080)
 
 # dataframe that holds usernames, passwords and other user data
